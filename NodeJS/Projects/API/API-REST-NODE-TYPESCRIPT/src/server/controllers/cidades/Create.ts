65eac2694e1b1b5d9dab7c39c6a5f2933a8eb6c6
Cleber Sanches <clebersanches59@gmail.com>
2024-05-09 19:55:04 -0300

    ✨ feat: add função de criar cidade

---
+++ NodeJS/Projects/API/API-REST-NODE-TYPESCRIPT/src/server/controllers/cidades/Create.ts
@@ -0,0 +1,21 @@
+
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
+import { validation } from '../../shared/middlewares';
+
+interface ICidade {
+  nome: string;
+}
+
+export const createValidation = validation((getSchema) => ({
+  body: getSchema<ICidade>(yup.object().shape({
+    nome: yup.string().min(3).required(),
+  })),
+}));
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+
+
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não Implementato!');
+}; 
